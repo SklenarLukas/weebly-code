c6eb4f8b0f0c88eeacedb858623ec8183ee02f5f
SklenarLukas <sklenar165@gmail.com>
2023-05-08 02:24:15 +0200

    folder struktura done povedzme

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
-<!-- 
-
-    <!DOCTYPE html>
-<html lang="sk">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-</head>
+<?php 
+include 'inc/header.php';
+$pageName = "Domov";
+?>
 
 <body>
     <div class="full-container">
