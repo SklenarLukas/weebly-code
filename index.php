322bdfb36bf95a20395adc03d681c494d25dd6de
SklenarLukas <sklenar165@gmail.com>
2023-05-08 19:44:55 +0200

    fixol som cesty

--- index.php
+++ index.php
@@ -33,15 +33,15 @@ $pageName = "Domov";
 
             <div class="services-list">
                 <div class="col-1">
-                    <img src="img/letenky.jpg" alt="">
+                    <img src="img/home-img/letenky.jpg" alt="">
                     <h2>Letenky</h2>
                 </div>
                 <div class="col-2">
-                    <img src="img/hotel.jpg" alt="">
+                    <img src="img/home-img/hotel.jpg" alt="">
                     <h2>Hotel</h2>
                 </div>
                 <div class="col-3">
-                    <img src="img/sprievodca.jpg" alt="">
+                    <img src="img/home-img/sprievodca.jpg" alt="">
                     <h2>Sprievodca</h2>
                 </div>
             </div>
@@ -54,137 +54,3 @@ $pageName = "Domov";
 
 
 
-
-
-
-
-
-
-
-
-<!--   
-    
-    
-    
-    <!DOCTYPE html>
-<html lang="sk">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-</head>
-
-<body>
-
-    <div class="bg">
-        <nav id="navbar" class="main-menu">
-            <ul class="prekliky">
-                <div class="logo">
-                    
-                </div>
-                <li><a class="uvod" href="index.html">Úvod</a></li>
-                <li><a class="onas" href="onas.html">O nás</a></li>
-                <li><a class="sluzby" href="sluzby.html">Služby</a></li>
-                <li><a class="kontakt" href="kontakt.html">Kontakt</a></li>
-                <li><a href="prihlasenie.html" class="button-prihlasit"><button class="prihlasit">PRIHLÁSIŤ</button></a>
-                </li>
-                <li><a href="registracia.html" class="button-registrovat"><button
-                            class="registrovat">REGISTROVAŤ</button></a></li>
-            </ul>
-        </nav>
-    </div>
-
-    <div class="container-home-sec1">
-        <div class="text-btn">
-            <p class="title-home">Dovolenka leto 2023</p>
-            <button>PREZERAŤ PONUKU</button>
-        </div>
-    </div>
-
-
-
-</body>
-
-</html>
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    <div class="navbar-container">
-        <nav>
-            <ul>
-                <li><a href="">Domov</a></li>
-                <li><a href="">Destinácie</a></li>
-                <li><a href="">First Minute</a></li>
-                <li><a href="">Ďalšie služby</a></li>
-                <li><a href="">Kontakt</a></li>
-
-            </ul>
-        </nav>
-    </div>
-
-
-
-
--->
-
-
-
-<!--
-
-    <!DOCTYPE html>
-<html lang="sk">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-</head>
-
-<body>
-    <div class="bg">
-        <nav id="navbar" class="main-menu">
-            <ul class="prekliky">
-                <div class="logo">
-                    <li><a href="" class="">
-                            <h1 class="zenit"><img src="img/logo.png" alt=""></h1>
-                        </a></li>
-                </div>
-                <li><a class="uvod" href="index.html">Domov</a></li>
-                <li><a class="onas" href="onas.html">Destinácie</a></li>
-                <li><a class="sluzby" href="sluzby.html">First Minute</a></li>
-                <li><a class="kontakt" href="kontakt.html">Ďalšie služby</a></li>
-                <li><a class="kontakt" href="kontakt.html">Kontakt</a></li>
-                <li><a href="prihlasenie.html" class="button-prihlasit"><button class="prihlasit">PRIHLÁSIŤ</button></a></li>
-                    <li><a href="registracia.html" class="button-registrovat"><button class="registrovat">REGISTROVAŤ</button></a></li>
-                </ul>
-            </nav>
-        </div>
-    
-    
-    
-    
-    </body>
-    
-    </html>
--->
