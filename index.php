a96cb1e9b308371e40b7658427d61936f24cebf2
SklenarLukas <sklenar165@gmail.com>
2023-05-09 02:05:52 +0200

    home page takmer done, už len doladiť detaily a urobiť DRY code

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include 'inc/header.php';
 $pageName = "Domov";
 ?>
@@ -21,36 +21,79 @@ $pageName = "Domov";
         <section class="main-page">
             <div class="banner-main-page">
                 <h2>Dovolenka leto 2023</h2>
-                <button>PREZERAŤ PONUKU</button>
+                <button>prezerať ponuku</button>
             </div>
         </section>
 
         <section class="services">
             <div class="banner-services">
-                <h2>NAŠE SLUŽBY</h2>
+                <h2>naše služby</h2>
                 <h3>Vám pomôžu pri cestovaní</h3>
             </div>
 
             <div class="services-list">
-                <div class="col-1">
+                <div class="col">
                     <img src="img/home-img/letenky.jpg" alt="">
                     <h2>Letenky</h2>
                 </div>
-                <div class="col-2">
+                <div class="col">
                     <img src="img/home-img/hotel.jpg" alt="">
                     <h2>Hotel</h2>
                 </div>
-                <div class="col-3">
+                <div class="col">
                     <img src="img/home-img/sprievodca.jpg" alt="">
                     <h2>Sprievodca</h2>
                 </div>
             </div>
+            <button>viac info</button>
+        </section>
+        <section class="chinese-wall">
+            <div class="title-field">
+                <p class="destinations-title">destinácie</p>
+                <h2>Veľký čínsky múr</h2>
+                <p>Je starý systém opevnenia, ktorý sa ťahá naprieč severnou Čínou. Jeho účelom bolo chrániť Čínu pred mongolskými vpádmi.
+                    Múr sa tiahne v dĺžke 8 850 km. V niektorých úsekoch prebieha v niekoľkých líniách a vytvára odbočky, na iných miestach je naopak prerušený.</p>
+                <button>zistiť viac</button>
+            </div>
+        </section>
+        <section class="tools">
+            <h4>inteligentné nástroje</h4>
+            <h2>Uľahčite si vaše hľadanie</h2>
+            <h6>Pre viac informácií kliknite na jednotlivé ikony</h6>
+            <div class="tools-list">
+                <div class="col">
+                    <img src="https://lsklenar.weebly.com/uploads/1/4/3/7/143782816/bell_1.jpg" alt="">
+                    <h5>Cenové upozornenie</h5>
+                    <p>Zapnite si cenové upozornenie a nenechajte si ujsť prijateľnú cenu vašej dovolenky.</p>
+                </div>
+                <div class="col">
+                    <img src="https://lsklenar.weebly.com/uploads/1/4/3/7/143782816/doc_1.jpg" alt="">
+                    <h5>Rýchle dokumenty</h5>
+                    <p>Stiahnite si potrebné dokumenty na cestovanie rýchlo a jednoducho bez potreby vypisovania údajov.</p>
+                </div>
+                <div class="col">
+                    <img src="https://lsklenar.weebly.com/uploads/1/4/3/7/143782816/plane_1.jpg" alt="">
+                    <h5>Poistenie</h5>
+                    <p>Prezrite si ceny všetkých dostupných cestovných poistení na jednom mieste a ušetrite svoj čas.</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="reviews">
+            <h4>recenzie</h4>
+            <h2>Spokojní zákazníci</h2>
+            <div>
+                <blockquote>"</blockquote>
+                <h6>Boli sme veľmi spokojní s pomerom ceny a kvality, hotel bol čistý a služby fungovali tak, ako mali. Veľmi sa nám páčil letiskový transfer, vodič prišiel načas. Určite túto CK odporúčam!</h6>
+                
+            </div>
+            <img src="https://lsklenar.weebly.com/uploads/1/4/3/7/143782816/stars_1.png" alt="">
+            <p>Ivana Bieliková</p>
         </section>
     </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
-
-
-
-
