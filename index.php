f98e4dc83d0627c8760e355557102f7add5ad838
z21ite21 <z21ite21@z00p3-01.SPSKNM>
2023-05-16 11:58:36 +0200

    fix main page v skole

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $pageName = "Domov";
             <div class="container text-center">
                 <h2>naše služby</h2>
                 <h3>Vám pomôžu pri cestovaní</h3>
-                <div class="row align-items-start">
+                <div class="row">
                     <div class="col">
                         <img src="img/home-img/letenky.jpg" class="img-fluid" alt="...">
                         <h4>Letenky</h4>
@@ -51,8 +51,10 @@ $pageName = "Domov";
             <div class="container title-field">
                 <p class="destinations-title">destinácie</p>
                 <h2>Veľký čínsky múr</h2>
-                <p>Je starý systém opevnenia, ktorý sa ťahá naprieč severnou Čínou. Jeho účelom bolo chrániť Čínu pred mongolskými vpádmi.
-                    Múr sa tiahne v dĺžke 8 850 km. V niektorých úsekoch prebieha v niekoľkých líniách a vytvára odbočky, na iných miestach je naopak prerušený.</p>
+                <p>Je starý systém opevnenia, ktorý sa ťahá naprieč severnou Čínou. Jeho účelom bolo chrániť Čínu pred
+                    mongolskými vpádmi.
+                    Múr sa tiahne v dĺžke 8 850 km. V niektorých úsekoch prebieha v niekoľkých líniách a vytvára
+                    odbočky, na iných miestach je naopak prerušený.</p>
                 <button>zistiť viac</button>
             </div>
         </section>
@@ -62,21 +64,26 @@ $pageName = "Domov";
                 <h4>inteligentné nástroje</h4>
                 <h2>Uľahčite si vaše hľadanie</h2>
                 <h6>Pre viac informácií kliknite na jednotlivé ikony</h6>
-                <div class="row align-items-start">
+                <div class="row">
                     <div class="col">
-                        <img src="https://lsklenar.weebly.com/uploads/1/4/3/7/143782816/bell_1.jpg" class="img-fluid" alt="...">
+                        <img src="https://lsklenar.weebly.com/uploads/1/4/3/7/143782816/bell_1.jpg" class="img-fluid"
+                            alt="...">
                         <h5>Cenové upozornenie</h5>
                         <p>Zapnite si cenové upozornenie a nenechajte si ujsť prijateľnú cenu vašej dovolenky.</p>
                     </div>
                     <div class="col">
-                        <img src="https://lsklenar.weebly.com/uploads/1/4/3/7/143782816/doc_1.jpg" class="img-fluid" alt="...">
+                        <img src="https://lsklenar.weebly.com/uploads/1/4/3/7/143782816/doc_1.jpg" class="img-fluid"
+                            alt="...">
                         <h5>Rýchle dokumenty</h5>
-                        <p>Stiahnite si potrebné dokumenty na cestovanie rýchlo a jednoducho bez potreby vypisovania údajov.</p>
+                        <p>Stiahnite si potrebné dokumenty na cestovanie rýchlo a jednoducho bez potreby vypisovania
+                            údajov.</p>
                     </div>
                     <div class="col">
-                        <img src="https://lsklenar.weebly.com/uploads/1/4/3/7/143782816/plane_1.jpg" class="img-fluid" alt="...">
+                        <img src="https://lsklenar.weebly.com/uploads/1/4/3/7/143782816/plane_1.jpg" class="img-fluid"
+                            alt="...">
                         <h5>Poistenie</h5>
-                        <p>Prezrite si ceny všetkých dostupných cestovných poistení na jednom mieste a ušetrite svoj čas.</p>
+                        <p>Prezrite si ceny všetkých dostupných cestovných poistení na jednom mieste a ušetrite svoj
+                            čas.</p>
                     </div>
                 </div>
             </div>
@@ -87,17 +94,23 @@ $pageName = "Domov";
             <div class="container">
                 <h4>recenzie</h4>
                 <h2>Spokojní zákazníci</h2>
-                <div>
+                <div class="reviews-text">
                     <blockquote>"</blockquote>
-                    <h6>Boli sme veľmi spokojní s pomerom ceny a kvality, hotel bol čistý a služby fungovali tak, ako mali. Veľmi sa nám páčil letiskový transfer, vodič prišiel načas. Určite túto CK odporúčam!</h6>
+                    <h6>Boli sme veľmi spokojní s pomerom ceny a kvality, hotel bol čistý a služby fungovali tak, ako
+                        mali. Veľmi sa nám páčil letiskový transfer, vodič prišiel načas. Určite túto CK odporúčam!</h6>
                 </div>
-                <img src="https://lsklenar.weebly.com/uploads/1/4/3/7/143782816/stars_1.png" class="img-fluid" alt="...">
-                <p>Ivana Bieliková</p>
+                <div class="stars">
+                    <img src="https://lsklenar.weebly.com/uploads/1/4/3/7/143782816/stars_1.png" class="img-fluid"
+                        alt="...">
+                    <p>Ivana Bieliková</p>
+                </div>
             </div>
         </section>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
