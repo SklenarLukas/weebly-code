1824f08890711639fe75c8c8593c71f7ef1de2af
SklenarLukas <sklenar165@gmail.com>
2023-05-29 00:31:54 +0200

    navbar edit

--- php-pages/another-services.php
+++ php-pages/another-services.php
@@ -4,20 +4,7 @@ include '../inc/header.php';
 ?>
 
 <body>
-    <header>
-        <nav class="container">
-            <ul>
-                <div class="logo">
-                    <img src="../img/logo.png" alt="">
-                </div>
-                <li><a href="../index.php">Domov</a></li>
-                <li><a href="../php-pages/destinations.php">Destinácie</a></li>
-                <li><a href="../php-pages/first-minute.php">First Minute</a></li>
-                <li><a href="../php-pages/another-services.php">Ďalšie služby</a></li>
-                <li><a href="../php-pages/contact.php">Kontakt</a></li>
-            </ul>
-        </nav>
-    </header>
+    <?php include '../inc/navbar.php' ?>
 
     <div class="main-wrap">
         <section class="our-services">
