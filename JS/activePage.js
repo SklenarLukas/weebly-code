42cb8f780cb3370cd11a2ab589e95b5df4678355
SklenarLukas <sklenar165@gmail.com>
2023-05-30 00:37:03 +0200

    JS edit

--- JS/activePage.js
+++ JS/activePage.js
@@ -1,9 +1,6 @@
 const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll("nav a").forEach(link => {
-    if (activePage === "/index.php") {
-        link.classList.add("active");
-    }
-    else if (link.href.includes(`${activePage}`)) {
+    if (link.href.includes(`${activePage}`)) {
         link.classList.add("active");
     }
 });
