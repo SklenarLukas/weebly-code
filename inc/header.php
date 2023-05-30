c066159ffdd7c56eea862c7b6f805528308b592a
z21ite21 <z21ite21@z00p3-01.SPSKNM>
2023-05-30 11:08:07 +0200

    favicon edit

--- inc/header.php
+++ inc/header.php
@@ -3,10 +3,12 @@
 
 <head>
     <meta charset="UTF-8">
- <link rel="shortcut icon" type="x-icon" href="favicon.ico">
+    <link rel="shortcut icon" type="x-icon" href="favicon.ico">
+    <link rel="shortcut icon" type="x-icon" href="../favicon.ico">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="./styles/main.css">
     <link rel="stylesheet" href="../styles/main.css">
     <link rel="stylesheet" href="../../styles/main.css">
@@ -14,5 +16,7 @@
     <script defer src="JS/navStyle.js"></script>
     <script defer src="../JS/activePage.js"></script>
     <script defer src="../JS/navStyle.js"></script>
-    <title><?php echo $pageName; ?></title>
+    <title>
+        <?php echo $pageName; ?>
+    </title>
 </head>
